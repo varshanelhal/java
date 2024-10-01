a5d2c559328aabde9d67279b6935c1bcd66824da
varshanelhal <chinnuvarshu24@gmail.com>
2024-10-01 15:41:40 +0530

    pushing code

---
+++ NotYetDecided.java
@@ -0,0 +1,11 @@
+public class NotYetDecided{
+
+
+         public static void main(String args[]) { 
+ 
+
+            System.out.println("not yet decided");
+
+         }   
+
+}
---
+++ SolarTrackingSystem.java
@@ -0,0 +1,9 @@
+public class SolarTrackingSystem{
+
+         public static void main(String args[]) {
+
+         System.out.println("solar tracking system");
+
+              }
+
+}          
